b3f579db86784bf692a25e212427c2128d433abe
Pr3stigent <83563230+Pr3stigent@users.noreply.github.com>
2021-12-21 17:20:16 -0400

    Create Debounce.ts

---
+++ src/shared/Debounce.ts
@@ -0,0 +1,37 @@
+interface validateData {
+    id: string, 
+    validateTime: number,
+    autoSet?: boolean,
+    conditionType?: ">=" | "<="
+}
+const getDebounce = (id: string) => script.GetAttribute(id)
+
+function setDebounce(id: string) {
+    script.SetAttribute(id, time())
+}
+
+function validateDebounce(data: validateData) {
+    const debounce = getDebounce(data.id)
+    let conditionTrue
+    
+    switch(data.conditionType) {
+        case ">=":
+            conditionTrue = time() - debounce >= data.validateTime
+        case "<=":
+             conditionTrue = time() - debounce <= data.validateTime
+        default:
+            conditionTrue = time() - debounce >= data.validateTime
+    }
+    
+    if (conditionTrue && data.autoSet) {
+        setDebounce(dqta.id)
+    }
+    
+    return conditionTrue
+}
+
+export = {
+    Get: getDebounce,
+    Set: setDebounce,
+    Validate: validateDebounce
+}
