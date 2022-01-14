b15b4329015380b69294090d0dfb44bbd5eb8a57
Pr3stigent <83563230+Pr3stigent@users.noreply.github.com>
2022-01-14 14:11:50 +0000

    Create NPC.ts

---
+++ src/client/Knit/Modules/NPC.ts
@@ -0,0 +1,21 @@
+import { Players } from "@rbxts/services"
+
+const npcTemplate = script.NPC
+
+class NPC {
+  private npc
+  private npcData
+  
+  constructor(npcData) {
+    this.npcData = npcData
+    
+  }
+  
+  public Move() {
+    
+  }
+  
+  public Destroy() {
+    
+  }
+}
